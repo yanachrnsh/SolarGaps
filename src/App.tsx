8346de2f2db40bd5d71893177f57495a73ea4db7
Yana <yanachernushevich@gmail.com>
2024-06-10 21:10:29 +0200

    added prettier and tw

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
 
 function App() {
-  
-  return (
-    <>
-    </>
-  )
+	return (
+		<div className="flex">
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel vitae error consequatur repellendus eligendi odit illo ut
+				rerum blanditiis inventore tempore, pariatur, veritatis quia aut. Nemo eaque odit voluptatibus accusantium!
+			</p>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel vitae error consequatur repellendus eligendi odit illo ut
+				rerum blanditiis inventore tempore, pariatur, veritatis quia aut. Nemo eaque odit voluptatibus accusantium!
+			</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
